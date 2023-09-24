b38498be2552460fd7f10268d6538242ba14d25a
Dawn <dydawnraine@gmail.com>
2023-09-24 19:35:19 +0800

    duration fix, minor css changes

--- src/App.js
+++ src/App.js
@@ -8,73 +8,112 @@ import LinearProgress from "@mui/material/LinearProgress";
 function App() {
   const [generatedNumber, setGeneratedNumber] = useState([]);
   const [queue, setQueue] = useState([[], [], [], []]);
-  const [currentQueue, setCurrentQueue] = useState(1);
-  const [countdown, setCountdown] = useState(false);
+  const [countdown, setCountdown] = useState([false, false, false, false]);
 
-  /* Getting of task number */
+  // Getting of task number
   const getNumber = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
 
+    // Generates a random number with in the min-max range
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    const highPriority = Math.random() < 0.25;
 
+    // Generates a boolean value of "high priority" to a number with 20% chance.
+    // True -> High priority
+    // False -> Regular priority
+    const highPriority = Math.random() < 0.2;
+
+    // Creates an object with 3 properties: the number itself, the duration
+    // (equal to the num itself) and the highPriority (true/false)
     const newNum = {
       number: num,
       duration: num,
       highPriority: highPriority,
     };
+
+    // Updates generatedNumber with the function taking
+    // the prev value. It adds a new number object to an existing list
     setGeneratedNumber((previousNum) => [...previousNum, newNum]);
   };
 
-  /* Admitting number to queue */
+  // Admitting number to queue
   const admitNumberToQueue = () => {
+    //Takes the first number of the list
     const generatedNum = generatedNumber[0];
 
+    //Checks if it is empty or not
     if (generatedNum) {
+      let updatedGeneratedNumber = [...generatedNumber];
+      let updatedQueue = [...queue];
+      let updatedCountdown = [...countdown];
+
       if (generatedNum.highPriority) {
-        setQueue((previousQueue) => {
-          const newQueue = [...previousQueue];
-          newQueue[0].push(generatedNum);
-          return newQueue;
-        });
+        updatedQueue[0].push(generatedNum);
+        updatedCountdown[0] = true;
       } else {
-        setQueue((previousQueue) => {
-          const newQueue = [...previousQueue];
-          newQueue[currentQueue].push(generatedNum);
-          return newQueue;
-        });
-        setCurrentQueue((prevQueue) => (prevQueue % 3) + 1);
+        const queueDurations = queue
+          .slice(1, 4)
+          .map((queueNumbers) =>
+            queueNumbers.reduce(
+              (totalDuration, numberObj) => totalDuration + numberObj.duration,
+              0
+            )
+          );
+
+        let shortestQueueIndex = queueDurations.indexOf(
+          Math.min(...queueDurations)
+        );
+
+        updatedQueue[shortestQueueIndex + 1].push(generatedNum);
+        updatedCountdown[shortestQueueIndex + 1] = true;
       }
 
-      setGeneratedNumber((previousNum) => previousNum.slice(1));
-      setCountdown(true);
+      updatedGeneratedNumber = updatedGeneratedNumber.slice(1);
+
+      setQueue(updatedQueue);
+      setCountdown(updatedCountdown);
+      setGeneratedNumber(updatedGeneratedNumber);
     }
   };
 
   const numberDuration = () => {
-    if (countdown) {
-      setQueue((previousQueue) => {
-        const newQueue = previousQueue.map((queueNumbers) =>
-          queueNumbers.map((numberObj) => {
-            if (numberObj.duration > 0) {
-              return { ...numberObj, duration: numberObj.duration - 1 };
-            }
-            return numberObj;
-          })
-        );
+    let updatedQueue = [...queue];
+    let updatedCountdown = [...countdown];
 
-        return newQueue.map((queueNumbers) =>
-          queueNumbers.filter((numberObj) => numberObj.duration > 0)
-        );
-      });
+    for (let queueIndex = 0; queueIndex < queue.length; queueIndex++) {
+      if (countdown[queueIndex] && queue[queueIndex].length > 0) {
+        const firstNumber = queue[queueIndex][0];
+
+        if (firstNumber.duration > 0) {
+          firstNumber.duration--;
+        }
+
+        if (firstNumber.duration === 0) {
+          updatedQueue[queueIndex].shift();
+        }
+
+        if (updatedQueue[queueIndex].length === 0) {
+          updatedCountdown[queueIndex] = false;
+        } else {
+          updatedCountdown[queueIndex] = true;
+        }
+      }
     }
+
+    setQueue(updatedQueue);
+    setCountdown(updatedCountdown);
   };
 
   useEffect(() => {
-    const interval = setInterval(numberDuration, 100);
+    const interval = setInterval(numberDuration, 50);
     return () => clearInterval(interval);
   }, [countdown]);
+
+  useEffect(() => {
+    if (queue.every((queueNumbers) => queueNumbers.length === 0)) {
+      setCountdown([false, false, false, false]);
+    }
+  }, [queue]);
 
   return (
     <div className="App">
@@ -85,7 +124,7 @@ function App() {
             variant="contained"
             aria-label="outlined primary button group"
           >
-            <Button variant="contained" onClick={() => getNumber(1, 100)}>
+            <Button variant="contained" onClick={() => getNumber(1, 200)}>
               Get Task
             </Button>
             <Button
@@ -124,6 +163,13 @@ function App() {
                 : `Regular Queue ${queueIndex}`}
             </h3>
             <LinearProgress
+              style={{
+                backgroundColor: "white",
+                marginTop: "1px",
+              }}
+              classes={{
+                bar: "progress-bar-fill",
+              }}
               variant="determinate"
               value={
                 (queueNumbers.reduce(
@@ -136,6 +182,7 @@ function App() {
               }
               sx={{ marginTop: 1, height: "10px" }}
             />
+
             <div>
               {queueNumbers.map((numberObj, index) => (
                 <div
